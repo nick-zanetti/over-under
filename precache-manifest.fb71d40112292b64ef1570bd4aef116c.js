74e70ab6c21fd9b4aeb6cf42df35a5a1d2dc1a31
Nick Zanetti <nzanetti44@gmail.com>
2020-05-09 11:57:56 -0400

    Updates

---
+++ precache-manifest.fb71d40112292b64ef1570bd4aef116c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7cadfaa69d1d33963a735c1cea39076",
+    "url": "/over-under/index.html"
+  },
+  {
+    "revision": "5bf36e6bb5d357d43236",
+    "url": "/over-under/static/css/main.c336b064.chunk.css"
+  },
+  {
+    "revision": "a1386ad7005528462c18",
+    "url": "/over-under/static/js/2.b36ae1cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/over-under/static/js/2.b36ae1cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bf36e6bb5d357d43236",
+    "url": "/over-under/static/js/main.ccab1daa.chunk.js"
+  },
+  {
+    "revision": "e277a2b341471ef14977",
+    "url": "/over-under/static/js/runtime-main.b3924c71.js"
+  }
+]);
